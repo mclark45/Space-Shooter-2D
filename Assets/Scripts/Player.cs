dc70a9d1b11f56fbf70b7ee97674e4bec20a4785
Matthew Clark <matthewspeed1@outlook.com>
2021-03-14 23:23:53 -0400

    adding player movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private int _speed;
+    [SerializeField]
+    private float _upperBoundary;
+    [SerializeField]
+    private float _lowerBoundary;
+    [SerializeField]
+    private float _leftBoundary;
+    [SerializeField]
+    private float _rightBoundary;
+    void Start()
+    {
+        transform.position = new Vector3(0, 0, 0);
+    }
+
+    
+    void Update()
+    {
+        CalculateMovement();
+    }
+
+    void CalculateMovement()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
+
+        transform.Translate(direction * _speed * Time.deltaTime);
+
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, _lowerBoundary, _upperBoundary), 0);
+
+        if (transform.position.x >= _rightBoundary)
+        {
+            transform.position = new Vector3(_leftBoundary, transform.position.y, 0);
+        }
+        else if (transform.position.x <= _leftBoundary)
+        {
+            transform.position = new Vector3(_rightBoundary, transform.position.y, 0);
+        }
+    }
+}
